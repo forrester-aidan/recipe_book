99408c3e8e98845fce93a858fb34753441c79ba7
forrester-aidan <forrester.aidan@ufl.edu>
2023-10-09 18:41:03 -0400

    begin sidebar next time

---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import { useState } from "react"
+
+export default function Navbar() {
+    const [showSidebar, setShowSidebar] = useState(false)
+
+    return (
+        <div className="navbar container">
+            <a href="#!" className="logo">Recipe<span>Book</span></a>
+            <div className="nav-links">
+                <a href="#!">Home</a>
+                <a href="#!">Recipes</a>
+                <a href="#!">Settings</a>
+            </div>
+            <div onClick={() => setShowSidebar(!showSidebar)} className={showSidebar ? "sidebar-btn active" : "sidebar-btn"}>
+                <div className="bar"></div>
+                <div className="bar"></div>
+                <div className="bar"></div>
+            </div>
+        </div>
+    )
+}
